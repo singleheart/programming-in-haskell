d4c410e470e7a7d7ac56880d7c201facfa3ea7ee
singleheart <singleheart@gmail.com>
2019-06-02 22:37:32 +0900

    remove duplicated file ex9.hs

--- ch4/ex9.hs
+++
@@ -1,8 +0,0 @@
-luhnDouble :: Int -> Int
-luhnDouble x =
-  if x * 2 > 9
-    then x * 2 - 9
-    else x * 2
-
-luhn :: Int -> Int -> Int -> Int -> Bool
-luhn a b c d = (luhnDouble a + b + luhnDouble c + d) `mod` 10 == 0
