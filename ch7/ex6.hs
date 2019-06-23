2d556288606a24c0169054f65ba0f49c06b7025b
singleheart <singleheart@gmail.com>
2019-06-23 21:31:17 +0900

    solve chapter 7 exercise 6

---
+++ ch7/ex6.hs
@@ -0,0 +1,18 @@
+type Bit = Int
+
+unfold :: (t -> Bool) -> (t -> a) -> (t -> t) -> t -> [a]
+unfold p h t x
+  | p x = []
+  | otherwise = h x : unfold p h t (t x)
+
+int2bin :: Int -> [Bit]
+int2bin = unfold (== 0) (`mod` 2) (`div` 2)
+
+chop8 :: [Bit] -> [[Bit]]
+chop8 = unfold null (take 8) (drop 8)
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = unfold null (f . head) tail
+
+iterate' :: (a -> a) -> a -> [a]
+iterate' = unfold (const False) id
