4a641421b092cc0ab0dd57d3c45158597b7be97d
singleheart <singleheart@gmail.com>
2019-06-23 21:57:17 +0900

    solve chapter 7 exercise 10

---
+++ ch7/ex10.hs
@@ -0,0 +1,12 @@
+altMap :: (a -> b) -> (a -> b) -> [a] -> [b]
+altMap _ _ [] = []
+altMap f0 f1 (x:xs) = f0 x : altMap f1 f0 xs
+
+luhnDouble :: Int -> Int
+luhnDouble x =
+  if x * 2 > 9
+    then x * 2 - 9
+    else x * 2
+
+luhn :: [Int] -> Bool
+luhn xs = sum (altMap luhnDouble id xs) `mod` 10 == 0
