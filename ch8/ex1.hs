a5e366faa798f43517d912f6a684dde1813e2484
singleheart <singleheart@gmail.com>
2019-07-06 18:16:48 +0900

    solve chapter 8 exercise 1

---
+++ ch8/ex1.hs
@@ -0,0 +1,20 @@
+data Nat
+  = Zero
+  | Succ Nat
+  deriving Show
+
+nat2int :: Nat -> Int
+nat2int Zero = 0
+nat2int (Succ n) = 1 + nat2int n
+
+int2nat :: Int -> Nat
+int2nat 0 = Zero
+int2nat n = Succ (int2nat (n - 1))
+
+add :: Nat -> Nat -> Nat
+add Zero n = n
+add (Succ m) n = Succ (add m n)
+
+mult :: Nat -> Nat -> Nat
+mult Zero _ = Zero
+mult (Succ m) n = add n (mult m n)
