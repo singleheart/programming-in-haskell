d18fc201440179530a1e6dd762a9dc9063d25f3b
singleheart <singleheart@gmail.com>
2019-07-06 18:51:27 +0900

    fix wrong condition

--- ch8/ex3.hs
+++ ch8/ex3.hs
@@ -4,8 +4,8 @@ data Tree a
 
 balanced :: Tree a -> Bool
 balanced (Leaf _) = True
-balanced (Node a b) = abs (size a - size b) <= 1
+balanced (Node l r) = abs (size l - size r) <= 1 && balanced l && balanced r
 
 size :: Tree a -> Int
 size (Leaf _) = 1
-size (Node a b) = size a + size b
+size (Node l r) = size l + size r
