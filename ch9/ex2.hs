d4faf42b9165bfc0864be7a2c26668012849dc4c
singleheart <singleheart@gmail.com>
2019-07-13 18:03:55 +0900

    revise exercise 9.2

--- ch9/ex2.hs
+++ ch9/ex2.hs
@@ -1,3 +1,9 @@
+removeOne :: Eq a => a -> [a] -> [a]
+removeOne _ [] = []
+removeOne x (y:ys)
+  | x == y = ys
+  | otherwise = y : removeOne x ys
+
 isChoice :: Eq a => [a] -> [a] -> Bool
 isChoice [] _ = True
-isChoice (x:xs) ys = x `elem` ys && isChoice xs ys
+isChoice (x:xs) ys = x `elem` ys && isChoice xs (removeOne x ys)
