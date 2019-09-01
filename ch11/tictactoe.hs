e7a1c63352cd31ea9a7ca6064bdd97eafa48d160
singleheart <singleheart@gmail.com>
2019-09-01 12:29:16 +0900

    implement 11.10 minimax algorithm

--- ch11/tictactoe.hs
+++ ch11/tictactoe.hs
@@ -147,3 +147,21 @@ prune n (Node x ts) = Node x [prune (n - 1) t | t <- ts]
 
 depth :: Int
 depth = 9
+
+minimax :: Tree Grid -> Tree (Grid, Player)
+minimax (Node g [])
+  | wins O g = Node (g, O) []
+  | wins X g = Node (g, X) []
+  | otherwise = Node (g, B) []
+minimax (Node g ts)
+  | turn g == O = Node (g, minimum ps) ts'
+  | turn g == X = Node (g, maximum ps) ts'
+  where
+    ts' = map minimax ts
+    ps = [p | Node (_, p) _ <- ts']
+
+bestmove :: Grid -> Player -> Grid
+bestmove g p = head [g' | Node (g', p') _ <- ts, p' == best]
+  where
+    tree = prune depth (gametree g p)
+    Node (_, best) ts = minimax tree
