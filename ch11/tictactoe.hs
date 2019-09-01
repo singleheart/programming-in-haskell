c77e70a8ae44997c93df29d0880ce5ffb0fbdb16
singleheart <singleheart@gmail.com>
2019-09-01 12:00:00 +0900

    implement 11.4 displaying a grid

--- ch11/tictactoe.hs
+++ ch11/tictactoe.hs
@@ -47,3 +47,24 @@ diag g = [g !! n !! n | n <- [0 .. size - 1]]
 
 won :: Grid -> Bool
 won g = wins O g || wins X g
+
+putGrid :: Grid -> IO ()
+putGrid = putStrLn . unlines . concat . interleave bar . map showRow
+  where
+    bar = [replicate ((size * 4) - 1) '-']
+
+showRow :: [Player] -> [String]
+showRow = beside . interleave bar . map showPlayer
+  where
+    beside = foldr1 (zipWith (++))
+    bar = replicate 3 "|"
+
+showPlayer :: Player -> [String]
+showPlayer O = ["   ", " O ", "   "]
+showPlayer B = ["   ", "   ", "   "]
+showPlayer X = ["   ", " X ", "   "]
+
+interleave :: a -> [a] -> [a]
+interleave x [] = []
+interleave x [y] = [y]
+interleave x (y:ys) = y : x : interleave x ys
