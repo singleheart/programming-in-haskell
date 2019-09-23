b72761ce8b30e46949db5ce7a73e958786fa9694
singleheart <singleheart@gmail.com>
2019-09-23 21:28:44 +0900

    implement 13.6 Derived primitives

--- ch13/parser.hs
+++ ch13/parser.hs
@@ -69,3 +69,58 @@ instance Alternative Parser
          case parse p inp of
            [] -> parse q inp
            [(v, out)] -> [(v, out)])
+
+sat :: (Char -> Bool) -> Parser Char
+sat p = do
+  x <- item
+  if p x
+    then return x
+    else empty
+
+digit :: Parser Char
+digit = sat isDigit
+
+lower :: Parser Char
+lower = sat isLower
+
+upper :: Parser Char
+upper = sat isUpper
+
+letter :: Parser Char
+letter = sat isAlpha
+
+alphanum :: Parser Char
+alphanum = sat isAlphaNum
+
+char :: Char -> Parser Char
+char x = sat (== x)
+
+string :: String -> Parser String
+string [] = return []
+string (x:xs) = do
+  char x
+  string xs
+  return (x : xs)
+
+ident :: Parser String
+ident = do
+  x <- lower
+  xs <- many alphanum
+  return (x : xs)
+
+nat :: Parser Int
+nat = do
+  xs <- some digit
+  return (read xs)
+
+space :: Parser ()
+space = do
+  many (sat isSpace)
+  return ()
+
+int :: Parser Int
+int =
+  do char '-'
+     n <- nat
+     return (-n)
+     <|> nat
