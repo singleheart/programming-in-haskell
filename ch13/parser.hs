b7b4ff33f02c87a750cd94a434d81809eae3fe8d
singleheart <singleheart@gmail.com>
2019-09-27 22:13:53 +0900

    implement 13.8 Arithmetic expressions

--- ch13/parser.hs
+++ ch13/parser.hs
@@ -154,3 +154,34 @@ nats = do
           natural)
   symbol "]"
   return (n : ns)
+
+expr :: Parser Int
+expr = do
+  t <- term
+  do symbol "+"
+     e <- expr
+     return (t + e)
+     <|> return t
+
+term :: Parser Int
+term = do
+  f <- factor
+  do symbol "*"
+     t <- term
+     return (f * t)
+     <|> return f
+
+factor :: Parser Int
+factor = do
+  symbol "("
+  e <- expr
+  symbol ")"
+  return e 
+  <|> natural
+
+eval :: String -> Int
+eval xs =
+  case (parse expr xs) of
+    [(n, [])] -> n
+    [(_, out)] -> error ("Unused input " ++ out)
+    [] -> error "Invalid input"
